3958137b097c176b3d673423c005608cde2fd27f
Zachary Scott <zachary@zacharyscott.net>
2014-02-20 11:41:21 +1100

    Add documentation for DRb parts.

--- druby/chat.rb
+++ druby/chat.rb
@@ -6,6 +6,12 @@ print "What's your username? "
 username = gets.chomp
 DRb.start_service
 
+# Create a new remote object stub.
+#
+# Since we're not stubbing a local object, we pass nil.
+#
+# Then connect to the druby server which returns a remote instance of the
+# Server object.
 remote_object = DRbObject.new(nil, remote)
 chat = User.new(username)
 entry = remote_object.add_user(chat)
--- druby/client.rb
+++ druby/client.rb
@@ -2,7 +2,11 @@ require 'drb/drb'
 require_relative 'server'
 
 class Client
-  # ri DRb::DRbUndumped
+  # Makes an instance of Client undumpable or unmarshallable.
+  #
+  # When a method any Client instance method is called over drb, then the
+  # object instance remains in the server space and a reference to the object
+  # is returned, rather than the object being marshalled.
   include DRbUndumped
 
   attr_accessor :username, :remote, :server
@@ -22,5 +26,19 @@ class Client
   end
 end
 
+# Start a dRuby server locally.
+#
+# After binding the drb server, Server is where remote method calls on the drb
+# server will be passed.
 DRb.start_service 'druby://localhost:12345', Server.new
+
+# The calling thread will suspend execution and run the thread for the druby
+# primary server.
+#
+# Does not return until the primary server thread exits
+# or until the given limit seconds have passed.
+#
+# If the time limit expires, nil will be returned, otherwise thr is returned.
+#
+# Any threads not joined will be killed when the main program exits.
 DRb.thread.join
